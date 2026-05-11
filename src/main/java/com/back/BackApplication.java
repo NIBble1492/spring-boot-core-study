e9ca716af64a454de1d3092f8b17b02a9595c18c
JUNHYEONG LEE <nibble1492@gmail.com>
2026-05-11 12:16:16 +0900

    Initial commit

---
+++ src/main/java/com/back/BackApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P141641Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P141641Application.class, args);
+    }
+
+}
